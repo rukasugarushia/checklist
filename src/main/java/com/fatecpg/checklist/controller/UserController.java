4ee19ae39fe1fd587c57f8776452b7499c690473
Lucas Garcia <lucasmartins7991@gmail.com>
2019-03-27 08:39:06 -0300

    fix

--- src/main/java/com/fatecpg/checklist/controller/UserController.java
+++ src/main/java/com/fatecpg/checklist/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.fatecpg.checklist.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fatecpg.checklist.model.User;
-import com.fatecpg.checklist.repository.UserRepository;
 import com.fatecpg.checklist.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,14 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.Map;
-
 @Controller
 @RequestMapping(path = "/user")
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
     private UserService userService;
 
     @GetMapping(path = "/add")
